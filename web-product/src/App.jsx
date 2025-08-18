ad9eb019beac050905093fd3d96f1eed44041150
Luis Gutiérrez <alberto.gutierrezm@gmail.com>
2025-08-18 17:48:48 -0400

    1.- configuracion inicial con Vite y primera componente funcional

---
+++ web-product/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='app-container'>
+      <h1>Hola!! este es mi proyecto de react</h1>
+      <p>esta es la base de nuestra aplicación.</p>
+    </div>
+  )
+}
+
+export default App
